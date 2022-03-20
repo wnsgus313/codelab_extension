e2c36c832b1eae5816f2d0174cb9ca7c637221b3
Junhyeon <wnsgus313@gmail.com>
2022-03-20 22:46:53 +0900

    fetch and save files

--- src/extension.ts
+++ src/extension.ts
@@ -7,17 +7,20 @@ import * as https from "https";
 import * as axios from "axios";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.downloadFile', () => {
+	let disposable = vscode.commands.registerCommand('extension.fetchAndSaveProblem', () => {
 		
-		const configParams = vscode.workspace.getConfiguration('download-file'),
-			defaultFolder = vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + configParams.get('defaultFolder') as string;
+		const configParamsUrl = vscode.workspace.getConfiguration('url'),
+			url = configParamsUrl.get('codeUrl') as string;
+		
+		const configParamsWS = vscode.workspace.getConfiguration('workspace'),
+			workSpaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath + '/';
 		
 		vscode.window.showInputBox({ prompt: 'Enter file URL you wish to download' }).then((res) => {
 		if (!res) {
-			vscode.window.showErrorMessage("Please enter valid URL!");
+			vscode.window.showErrorMessage("Please enter problem ID!");
 			return;
 		}
-		fetchAndSaveFile(res, defaultFolder);
+		fetchAndSaveFile(url+res, parseInt(res), workSpaceFolder + res);
 		});
 	});
 	context.subscriptions.push(disposable);
@@ -25,7 +28,7 @@ export function activate(context: vscode.ExtensionContext) {
 	disposable = vscode.commands.registerCommand('extension.uploadProblem', () => {
 		
 		const configParamsUrl = vscode.workspace.getConfiguration('url'),
-			url = configParamsUrl.get('uploadCode') as string;
+			url = configParamsUrl.get('codeUrl') as string;
 
 		const configParamsWS = vscode.workspace.getConfiguration('workspace'),
 			workSpaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath + '/';
@@ -42,11 +45,11 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function uploadProblem(url:string, problemId:number, workSpaceFolder:string) {
+function uploadProblem(url:string, problemId:number, targetPath:string) {
 	const axios = require('axios');
-	console.log(workSpaceFolder); // /Users/junhyeonbae/Desktop/vscode연습/백준문제풀이/1
+	console.log(targetPath); // /Users/junhyeonbae/Desktop/vscode연습/백준문제풀이/1
 
-	let fileLists:string[] = fs.readdirSync(workSpaceFolder);
+	let fileLists:string[] = fs.readdirSync(targetPath);
 
 	let files = {
 		'filename': '',
@@ -57,7 +60,7 @@ function uploadProblem(url:string, problemId:number, workSpaceFolder:string) {
 
 	fileLists.forEach((file) => {
 		files['filename'] = file;
-		files['file'] = fs.readFileSync(workSpaceFolder+'/'+file, "binary");
+		files['file'] = fs.readFileSync(targetPath+'/'+file, "binary");
 
 		axios.post(url, {files})
 		.then((res:any) => {
@@ -68,53 +71,29 @@ function uploadProblem(url:string, problemId:number, workSpaceFolder:string) {
 	});
 }
 
-function fetchAndSaveFile(fileURL:string, dest:string) {
-	const timeout = 10000,
-		urlParsed = url.parse(fileURL),
-		uri = urlParsed.pathname.split('/');
-	let req,
-		filename = (uri[uri.length - 1].match(/(\w*\.?-?)+/))[0];
+function fetchAndSaveFile(url:string, problemId:number, targetPath:string) {
+	const axios = require('axios');
+	console.log(targetPath);
 
-	if (urlParsed.protocol === null) {
-		fileURL = 'http://' + fileURL;
-	}
-
-	req = (urlParsed.protocol === 'https:') ? https : http; 
-
-	let request = req.get(fileURL, function(response) {
-		
-		// Make sure extension is present (mostly for images)
-		if (filename.indexOf('.') < 0) {
-			const contentType = response.headers['content-type'];
-			filename += `.${contentType.split('/')[1]}`;
+	axios.get(url)
+	.then((res:any) => {
+		if(!fs.existsSync(targetPath)){
+			fs.mkdirSync(targetPath);
 		}
 
-		const targetPath = `${dest}/${filename}`;
-
-		if (response.statusCode === 200) {
-
-		mkdirp(dest, function(err: any) { 
-			if (err) {
-				throw err;
-			}
-			var file = fs.createWriteStream(targetPath);
-			response.pipe(file);
+		res.data['file_list'].forEach((filename:string) => {
+			const saveFilePath = targetPath + '/' + filename;
+			axios.get(url + '/' + filename)
+			.then((res:any) => {
+				fs.writeFileSync(saveFilePath, res.data);
+			})
+			.catch((err:any) => {
+				vscode.window.showErrorMessage(`Fail save ${filename} in Problem ${problemId}`);
+			});
 		});
 
-		} else {
-			vscode.window.showErrorMessage(`Downloading ${fileURL} failed`);
-		}
-
-		response.on("end", function(){
-			vscode.window.showInformationMessage(`File "${filename}" downloaded successfully.`);
-		});
-
-		request.setTimeout(timeout, function () {
-			request.abort();
-		});
-
-	}).on('error', function(e) {
-		vscode.window.showErrorMessage(`Downloading ${fileURL} failed! Please make sure URL is valid.`);
+	}).catch((err:any) => {
+		vscode.window.showErrorMessage(`Please check Problem Id : ${problemId}`);
 	});
 }
 
