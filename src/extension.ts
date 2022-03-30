185eb03b7508d79bcc2768ce7c13ee36f91b56e1
Junhyeon <wnsgus313@gmail.com>
2022-03-30 12:26:18 +0900

    sign up

--- src/extension.ts
+++ src/extension.ts
@@ -28,8 +28,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 
+
 	disposable = vscode.commands.registerCommand('extension.uploadProblem', () => {
-		
 		const configParamsUrl = vscode.workspace.getConfiguration('url'),
 			url = configParamsUrl.get('problemsUrl') as string;
 
@@ -47,8 +47,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 
+
 	disposable = vscode.commands.registerCommand('extension.deleteProblem', () => {
-		
 		const configParamsUrl = vscode.workspace.getConfiguration('url'),
 			url = configParamsUrl.get('problemsUrl') as string;
 
@@ -66,8 +66,35 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 
+
 	disposable = vscode.commands.registerCommand('extension.submitCode', () => {
+		const configParamsUrl = vscode.workspace.getConfiguration('url'),
+			url = configParamsUrl.get('codesUrl') as string;
+
+		const configParamsWS = vscode.workspace.getConfiguration('workspace'),
+			workSpaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath + '/';
 		
+		
+		vscode.window.showInputBox({ prompt: 'Enter the problem ID you want to code upload.' }).then((res) => {
+		if (!res) {
+			vscode.window.showErrorMessage("Please enter problem ID!");
+			return;
+		}
+		submitCode(url + '21700332/' + res, res, workSpaceFolder + res);
+		});
+	});
+	context.subscriptions.push(disposable);
+
+
+	disposable = vscode.commands.registerCommand('extension.codelabAuthentication', () => {
+		const configParamsUrl = vscode.workspace.getConfiguration('url'),
+			url = configParamsUrl.get('rootUrl') as string;
+		
+		if (url) {vscode.env.openExternal(vscode.Uri.parse(url));}
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('extension.codelabLogin', () => {
 		const configParamsUrl = vscode.workspace.getConfiguration('url'),
 			url = configParamsUrl.get('codesUrl') as string;
 
