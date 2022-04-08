59ac3cdb69bae054805e34c989766baad661cbff
Junhyeon <wnsgus313@gmail.com>
2022-04-08 11:00:45 +0900

    make treeView.ts

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import {uploadProblem, fetchAndSaveProblem, deleteProblem, fetchProblemContent, fetchProblemList} from './problems';
 import {submitCode} from './codes';
 import {askUserForSave} from './data';
+import { SolvedProblems } from './treeView';
 
 
 export function activate(context: vscode.ExtensionContext) {
@@ -177,7 +178,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	// problem list 새로고침
-	disposable = vscode.commands.registerCommand('problemProvider.refreshProblems', () => {
+	disposable = vscode.commands.registerCommand('problemProvider.refreshProblems', async () => {
 		let url:string | undefined;
 		if (info.get('url')) {
 			url = info.get('url') + problemListUrl;
@@ -186,98 +187,18 @@ export function activate(context: vscode.ExtensionContext) {
 		const configParamsWS = vscode.workspace.getConfiguration('workspace'),
 			workSpaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath + '/';
 
-		fetchProblemList(url, workSpaceFolder + '/problem_list.json', info);
+		await fetchProblemList(url, workSpaceFolder + '/problem_list.json', info);
+
+		const solvedProblems = new SolvedProblems(rootPath);
+		vscode.window.registerTreeDataProvider('solved', solvedProblems);
+		solvedProblems.refresh();
+
 	});
 	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 
-export class SolvedProblems implements vscode.TreeDataProvider<Dependency> {
-	constructor(private workspaceRoot: string) { }
-
-	getTreeItem(element: Dependency): vscode.TreeItem {
-		return element;
-	}
-
-	getChildren(element?: Dependency): Thenable<Dependency[]> {
-		if (!this.workspaceRoot) {
-			vscode.window.showInformationMessage('No dependency in empty workspace');
-			return Promise.resolve([]);
-		}
-
-		if (element) {
-			return Promise.resolve(
-				this.getDepsInPackageJson(
-					path.join(this.workspaceRoot, 'node_modules', 'problem_list.json')
-				)
-			);
-		} else {
-			const packageJsonPath = path.join(this.workspaceRoot, 'problem_list.json');
-			if (this.pathExists(packageJsonPath)) {
-				return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
-			} else {
-				vscode.window.showInformationMessage('Workspace has no problem_list.json');
-				return Promise.resolve([]);
-			}
-		}
-	}
-
-	/**
-	 * Given the path to package.json, read all its dependencies and devDependencies.
-	 */
-	private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
-		if (this.pathExists(packageJsonPath)) {
-			const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
-			const toDep = (moduleName: string, version: string): Dependency => {
-				if (this.pathExists(path.join(this.workspaceRoot, 'node_modules'))) {
-					return new Dependency(
-						version,
-						vscode.TreeItemCollapsibleState.Collapsed
-					);
-				} else {
-					return new Dependency(version, vscode.TreeItemCollapsibleState.None);
-				}
-			};
-
-			const deps = packageJson.Solved_Problems
-				? Object.keys(packageJson.Solved_Problems).map(dep =>
-					toDep(dep, packageJson.Solved_Problems[dep])
-				)
-				: [];
-			return deps;
-		} else {
-			return [];
-		}
-	}
-
-	private pathExists(p: string): boolean {
-		try {
-			fs.accessSync(p);
-		} catch (err) {
-			return false;
-		}
-		return true;
-	}
-
-	private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | null | void> = new vscode.EventEmitter<Dependency | undefined | null | void>();
-	readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | null | void> = this._onDidChangeTreeData.event;
-
-	refresh(): void {
-		this._onDidChangeTreeData.fire();
-	}
-
-}
-
-class Dependency extends vscode.TreeItem {
-	constructor(
-		private version: string,
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState
-	) {
-		super(version, collapsibleState);
-		this.tooltip = `${this.version}`;
-	}
-}
