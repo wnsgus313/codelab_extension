aeb125e2f122548c5367ef1ab937ba9effcf7d17
Junhyeon <wnsgus313@gmail.com>
2022-04-29 15:51:16 +0900

    video upload

--- src/videos.ts
+++ src/videos.ts
@@ -7,12 +7,18 @@ import FormData = require('form-data');
 export async function uploadVideo(url:string, title:string, targetPath:string, info:vscode.Memento) {
     const formData = new FormData();
     const token = await info.get('token');
-
-	console.log(targetPath); // /Users/junhyeonbae/Desktop/vscode연습/백준문제풀이/Bracket
-	console.log(url); // http://203.245.41.143:5000/api/v1/video/Bracket
     
-    path.join(targetPath, 'test.mp4');
-    formData.append('file', fs.createReadStream(path.join(targetPath, 'test.mp4')), 'video.mp4');
+    let fileLists:string[] = fs.readdirSync(targetPath);
+
+    fileLists.forEach((file) => {
+        let reg = /(.*?)\.(mp4|avi)$/;
+        if (file.match(reg)){
+            formData.append('file', fs.createReadStream(path.join(targetPath, file)), file);
+        }
+	});
+
+
+    // formData.append('file', fs.createReadStream(path.join(targetPath, 'test.mp4')), 'video.mp4');
     const auth = 'Basic ' + Buffer.from(token + ':').toString('base64');
 
     got.post(url, {
